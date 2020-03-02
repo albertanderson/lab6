0c25fa3f0421fd568dfcaed69ee6822b993305d6
albert <albertanderson122@gmail.com>
2020-03-02 17:53:39 +0000

    first commit

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import{Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/5530c1b6-5ca1-11ea-b3a3-0146f2530bbe');
+
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
